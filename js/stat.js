939bebcb3a596b3f683bd78dad9ec1e1560ac76a
lyukorzh <lyukorzh@gmail.com>
2020-07-04 01:42:21 +0300

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -1,1 +1,10 @@
 
+'use strict'
+var canvas = document.getElementById('mycanvas');
+var ctx = canvas.getContext('2d');
+fillStyle = 'rgba(0, 0, 0, 0.3)';
+ctx.fillRect(110, 20, 420, 270);
+fillStyle = '#fff';
+ctx.fillRect(100, 10, 420, 270);
+
+
