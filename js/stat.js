4cfcd897ee20f5b668c54b8f65cfd968fa9788e9
lyukorzh <lyukorzh@gmail.com>
2020-07-07 19:03:07 +0300

    в ветке мастер своего форка создала  папку js/stat.js, подключила файл в  index.html  перед js/game.js . В js/stat.js  объявляю константы : var CLOUD_WIDTH = 420; - ширина облака,  var CLOUD_HEIGHT = 270; - высота облака, var CLOUD_X = 100; -  начало координаты по оси Х - для облака, var CLOUD_Y = 10; -начало коордират по оси У для облака,  var GAP = 10; - отступ,  var FONT_GAP = 16; - высота строки, var HEIGHT_TEXT = 80 высота текста вверху облака; var BAR_HEIGTH = 150; - максимальная высота гистограммы,  var BAR_WIDTH = 40; - ширина гистограммы,  var BAR_IDENT = 50; - расстояние между  колонками. Далее пишу функцию отрисовки облака:  var renderCloud = function (ctx, x, y, color) {   ctx.fillStyle = color;   ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT); };.   Следующая функция находит максимальный  элемент массива:  var getMaxElement = function (arr) {   var maxElement = arr[0];   for (var i = 0; i < arr.length; i += 1) {     if (arr[i] > maxElement) {       maxElement = arr[i];     }   }   return maxElement; }; .   Подключаю функцию window.renderStatistics = function (ctx) {  "рисую нижнее облако серого цвета" renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');  "рисую верхнее облако белого циета" renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');   "текст вверху белого облака" ctx.fillStyle = 'rgba(0, 0, 0, 1)';   ctx.font = '16px sens-serif';   ctx.textBaseline = 'hanging';   ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP);   ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP + GAP);  "записываю максимальный элемент массива в переменную тоесть нахожу максимальное время прохождения игры"  var maxTime = getMaxElement(time);  " перебираю массив игроков" for (var i = 0; i < players.length; i += 1) {   "нахожу высоту гистограммы для каждого игрока"  var barHeight = Math.floor(BAR_HEIGTH * time[i]) / maxTime;  "текст"   ctx.fillStyle = 'rgba(0, 0, 0, 1)';  " над гистограммой "  ctx.fillText('Point' + (1 + i), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_IDENT) * i, CLOUD_Y + HEIGHT_TEXT + BAR_HEIGTH - barHeight - GAP - FONT_GAP);   " под гистограммой "  ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_IDENT) * i, CLOUD_HEIGHT - GAP);  " задаю цвет гистограммы для первого игрока "   if (i === 0) {       ctx.fillStyle = 'rgba(255, 0, 0, 1)';     } else " для остальных игроков "{       ctx.fillStyle = 'hsl(226, 91%, 41%)';     }    " рисую гистаграммы " ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_IDENT) * i, CLOUD_Y + HEIGHT_TEXT + BAR_HEIGTH - barHeight, BAR_WIDTH,         barHeight);   } };

--- js/stat.js
+++ js/stat.js
@@ -41,7 +41,7 @@ window.renderStatistics = function (ctx) {
   for (var i = 0; i < players.length; i += 1) {
     var barHeight = Math.floor(BAR_HEIGTH * time[i]) / maxTime;
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText('Point' + (1 + i), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_IDENT) * i, CLOUD_Y + HEIGHT_TEXT + BAR_HEIGTH - barHeight - GAP - FONT_GAP);
+    ctx.fillText('' + (1 + i), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_IDENT) * i, CLOUD_Y + HEIGHT_TEXT + BAR_HEIGTH - barHeight - GAP - FONT_GAP);
     ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_IDENT) * i, CLOUD_HEIGHT - GAP);
     if (i === 0) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
